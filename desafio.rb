7d75b2dc4d8b284e0c893eeacdff646b010d5f57
Willams Ribeiro <willamsribeiro1987@gmail.com>
2023-07-28 00:23:49 -0300

    primeiro commit Ruby

---
+++ desafio.rb
@@ -0,0 +1,11 @@
+# nome do usuário
+print "Digite seu nome: "
+nome = gets.chomp
+# sobrenome do usuário
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+# idade do usuário
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+# imprima numa única frase
+puts "#{nome} #{sobrenome}, #{idade} anos . "
